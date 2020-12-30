f5cc75eb13cc6ead5eba3a3c40b217b58c2a12a7
Skylaunch <maximkagrig@gmail.com>
2020-12-30 19:58:37 +0300

    Simple Xml parser
    Тестовое задание для вакансии hh

---
+++ GrigorevMaxim/GrigorevMaxim/GrigorevMaxim/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using HtmlAgilityPack;
+using Fizzler;
+using Fizzler.Systems.HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Xamarin.Forms;
+using System.Web;
+
+namespace GrigorevMaxim
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            currentIdEditor.Text = "";
+            GetXml(@"https://yastatic.net/market-export/_/partner/help/YML.xml");
+
+        }
+
+
+        XmlNodeList collections;
+        public void GetXml(string adress)
+        {
+            var xml = new WebClient().DownloadString(adress);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+            var page = doc.DocumentElement;
+            collections = page.SelectNodes("//yml_catalog/shop/offers/offer");
+            List<string> ids = new List<string>();
+            foreach (XmlNode item in collections)
+            {
+                ids.Add(item.Attributes["id"].Value);
+            }
+
+            OutputList.ItemsSource = ids;
+        }
+
+        private void OutputList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            foreach (XmlNode item in collections)
+            {
+                if (item.Attributes["id"].Value == OutputList.SelectedItem.ToString())
+                {
+                    currentIdEditor.Text = JsonConvert.SerializeObject(item);
+                }
+            }
+        }
+    }
+}
